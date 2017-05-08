15e0ed97ee6d8cd9fc4be0a6d48cd0812a25fb94
martinfroberg <martin.froberg@gmx.com>
2017-05-08 14:39:25 +0200

    Yaml4

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php require_once 'core/database/connect.php';
 $connection = db_connect(); ?>
 
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    hi
-  </body>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <title></title>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+  <!-- mobile viewport optimisation -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- stylesheets -->
+  <link rel="stylesheet" href="css/base.css" type="text/css"/>
+  <link rel="stylesheet" href="css/styles.css" type="text/css"/>
+  <!--[if lte IE 7]>
+  <link rel="stylesheet" href="css/iehacks.css" type="text/css"/>
+  <![endif]-->
+  <!--[if lt IE 9]>
+  <script src="js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body>
+  
+</body>
 </html>
