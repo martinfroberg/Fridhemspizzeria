83b434b0846db746060ed824e1b49e2eb761159e
martinfroberg <martin.froberg@gmx.com>
2017-05-08 14:25:45 +0200

    Grund

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require_once 'core/database/connect.php';
+$connection = db_connect(); ?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    hi
+  </body>
+</html>
