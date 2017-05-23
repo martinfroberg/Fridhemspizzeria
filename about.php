9c0f825e494d04711f965946c9bc77b7a1f741ec
danneelitz <danneelitz@gmail.com>
2017-05-23 15:20:49 +0200

    Bytt bakgrund och lagt grund för om oss fliken
    Ändrade bakgrund på sidan och fixat en grund för om oss sidan med
    google maps inlagt (fliken inte klar)

--- about.php
+++ about.php
@@ -6,7 +6,7 @@ $db = Database::getConnection(); ?>
 <html lang="sv">
 <head>
   <meta charset="utf-8"/>
-  <title>Pizzeria Fridhem - Meny</title>
+  <title>Pizzeria Fridhem - Om oss</title>
 
   <meta name="description" content=""/>
   <meta name="author" content=""/>
@@ -50,6 +50,47 @@ $db = Database::getConnection(); ?>
 
   </header>
   <!-- Body -->
+  
+  
+   <section class="ombager">
+    
+   <img class="bager1" src="images/bagersersap1.jpeg">
+   
+   <article class="bager">
+    
+   <p>Namn: Bager Sersap</p>
+   <p>Yrke: Pizzabagare, Ägare av Pizzeria Fridhem</p>
+   </article>
+   
+   </section> 
+   <article class="pizzeriafridhem">
+    <h2>Om Pizzeria Fridhem</h2>
+    
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet neque sit amet magna rutrum hendrerit ut eget est. Nunc vel dui eu libero pretium sagittis.
+    Aliquam nec consequat augue. Sed bibendum dui neque, a euismod ligula tempor ac. Etiam sed tristique magna, in sodales lectus. In porta nisl a finibus aliquam. Maecenas a metus ac tortor imperdiet tincidunt.
+    Nunc magna elit, sodales et tellus ut, porta rutrum enim. Sed et magna id dui commodo scelerisque. Ut suscipit sapien eget eros placerat efficitur.
+    Fusce eu arcu ultricies, varius tortor a, elementum neque. Curabitur est nibh, malesuada id pretium sit amet, ullamcorper at justo. Nam pharetra imperdiet rhoncus. Nullam rutrum finibus metus, vitae maximus ligula lacinia ut.
+    Fusce id velit elementum, aliquet nisi a, hendrerit velit. Sed ut ex tristique, sodales est a, feugiat nisi.
+
+    Mauris neque ipsum, laoreet non ex nec, sodales varius nulla. Maecenas at nunc neque. Suspendisse sed vehicula leo, at facilisis nisi.
+    Sed malesuada purus molestie, viverra libero et, condimentum est. Mauris mi diam, accumsan in varius quis, dapibus eget magna. Proin molestie, ligula sed imperdiet bibendum, enim libero accumsan turpis, id pharetra justo nibh vel orci.
+    Cras vel consectetur nibh. Proin vehicula elit dolor, in.</p>
+   </article>
+   
+  
+  
+   <div id="map"></div>
+      
+      
+        <script async defer
+         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-hvHiQl9w-Bqs_EdXHjDSHVbKm9nLGRg&callback=initMap">
+        </script>
+    <article class="pizzeriafridhem2" >
+      
+      <h2>Välkommen till Pizzeria Fridhem</h2>
+      
+      
+    </article>
 
   <footer>
      
@@ -65,13 +106,7 @@ $db = Database::getConnection(); ?>
              <p>© Pizzeria Fridhem<p>
          </article>
          
-        <div id="map"></div>
-      
-      
-        <script async defer
-         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-hvHiQl9w-Bqs_EdXHjDSHVbKm9nLGRg&callback=initMap">
-        </script>
-      
+       
         
   </footer>
 </body>
--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 
 function initMap() {
- var uluru = {lat: 56.186731, lng: 14.857781};
+ var PizzeriaFridhem = {lat: 56.186731, lng: 14.857781};
   var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 14,
-  center: uluru
+  center: PizzeriaFridhem
 });
   var marker = new google.maps.Marker({
-     position: uluru,
+     position: PizzeriaFridhem,
      map: map
  });
 }
